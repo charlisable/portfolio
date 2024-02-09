784669b170565bd0735bae48804fac63f5f5e55e
Charli <charlisable@gmail.com>
2024-02-09 13:49:22 -0600

    fixed svg hover, replaced resume

--- script.js
+++ script.js
@@ -20,53 +20,6 @@ mainBody.addEventListener('click', function() {
 	navMenu.classList.remove('active');
 })
 
-// When HTML svg hover, turn color black
-const htmlSvg = document.getElementById('html-svg');
-const htmlPath = document.getElementById('html-path');
-
-htmlSvg.addEventListener('mouseover', function(){
-	htmlPath.classList.add('fill-black');
-});
-htmlSvg.addEventListener('mouseout', function(){
-	htmlPath.classList.remove('fill-black');
-});
-
-// When CSS svg hover, turn color black
-const cssSvg = document.getElementById('css-svg');
-const cssPath = document.getElementById('css-path');
-
-cssSvg.addEventListener('mouseover', function(){
-	cssPath.classList.add('fill-black');
-});
-cssSvg.addEventListener('mouseout', function(){
-	cssPath.classList.remove('fill-black');
-});
-
-// When JS svg hover, turn color black
-const jsSvg = document.getElementById('js-svg');
-const jsPath = document.getElementById('js-path');
-
-jsSvg.addEventListener('mouseover', function(){
-	jsPath.classList.add('fill-black');
-});
-jsSvg.addEventListener('mouseout', function(){
-	jsPath.classList.remove('fill-black');
-});
-
-// When git svg hover, turn color black
-const gitSvg = document.getElementById('git-svg');
-const gitPath = document.getElementById('git-path');
-
-gitSvg.addEventListener('mouseover', function(){
-	gitPath.classList.add('fill-black');
-});
-gitSvg.addEventListener('mouseout', function(){
-	gitPath.classList.remove('fill-black');
-});
-
-
-
-
 
 
 // function for when submit has been pressed
